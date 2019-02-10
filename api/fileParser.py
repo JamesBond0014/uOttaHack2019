4ec996ffffe87d39ceca98e896f3d7eb32dae6c9
baojame2 <baojame2@teach.cs.utoronto.ca>
2019-02-10 10:39:22 -0500

    important files

---
+++ api/fileParser.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 14:45:28 2019
+
+@author: james
+"""
+import pandas as pd
+import json
+
+df = pd.read_csv("WasteWizard_Item_Description.csv")
+a = []
+b = []
+c = 0
+for i,row in df.iterrows():
+    d = {}
+    d["title"] = row["TITLE"]
+    
+
+    d["keywords"] = row["TITLE"] +", "+ str(row["ALT_WORDS"])
+    d["description"] = row["DESCRIPTION"]
+    a.append(d)
+    
+
+
+string= ""
+for i in a:
+    
+    string += json.dumps(i)
+    string += "UNIQUESTRINGSPLIT"
+
+
+string = string[:-1]
+
+
+file = open("parsedWizard2.txt","w")
+file.write(string)
+file.close()
